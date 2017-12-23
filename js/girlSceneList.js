8b306be14e1d45c4eebd26c1471e8351e23b1792
ao <dawn@ptmind.com>
2017-12-23 18:24:17 +0800

    html

--- js/girlSceneList.js
+++ js/girlSceneList.js
@@ -1,19 +1,116 @@
 var sceneList = [{
     name: 'loading.s1',
-    autoFinshTime: 3000, // 自动完成改场景
+    autoFinshTime: 1000, // 自动完成改场景
 }, {
-    name: "p1.s1",
-    start: function() {}, //场景开始的回调
+    name: "p0",
+    // start: function() {}, //场景开始的回调
     autoFinshTime: 1000, // 自动完成改场景
     enterTrigger: {
         type: 'last.finish'
     }
 }, {
-    name: "p3.s2",
+    name: "p1",
+    autoFinshTime: 1000, // 自动完成改场景
     enterTrigger: {
-        type: 'event',
-        eventName: 'boy.p2.s0.finish'
+        type: 'last.finish'
     }
+}, {
+    name: "p2.s10",
+    autoFinshTime: 1000, // 自动完成改场景
+    enterTrigger: {
+        type: 'last.finish'
+    }
+}, {
+    name: "p2.s20",
+    autoFinshTime: 1000, // 自动完成改场景
+    enterTrigger: {
+        type: 'last.finish'
+    }
+}, {
+    name: "p2.s30",
+    autoFinshTime: 1000, // 自动完成改场景
+    enterTrigger: {
+        type: 'last.finish'
+    }
+}, {
+    name: "p2.s40",
+    autoFinshTime: 1000, // 自动完成改场景
+    enterTrigger: {
+        type: 'last.finish'
+    }
+}, {
+    name: "p2.s50",
+    autoFinshTime: 1000, // 自动完成改场景
+    enterTrigger: {
+        type: 'last.finish'
+    }
+}, {
+    name: "p2.s60",
+    autoFinshTime: 1000, // 自动完成改场景
+    enterTrigger: {
+        type: 'last.finish'
+    }
+}, {
+    name: "p2.s70",
+    autoFinshTime: 1000, // 自动完成改场景
+    enterTrigger: {
+        type: 'last.finish'
+    }
+}, {
+    name: "loading.s1",
+    autoFinshTime: 2000, // 自动完成改场景
+    enterTrigger: {
+        type: 'last.finish'
+    }
+}, {
+    name: "p3",
+    autoFinshTime: 1500, // 自动完成改场景
+    enterTrigger: {
+        type: 'last.finish'
+    }
+}, {
+    name: "p4.s10",
+    autoFinshTime: 1000, // 自动完成改场景
+    enterTrigger: {
+        type: 'last.finish'
+    }
+}, {
+    name: "p4.s20",
+    autoFinshTime: 1000, // 自动完成改场景
+    enterTrigger: {
+        type: 'last.finish'
+    }
+}, {
+    name: "p4.s30",
+    // autoFinshTime: 1000, // 自动完成改场景
+    enterTrigger: {
+        type: 'last.finish'
+    }
+}, {
+    name: "p4.s40",
+    // autoFinshTime: 1000, // 自动完成改场景
+    // enterTrigger: {
+    //     type: 'last.finish'
+    // }
+}, {
+    name: "p4.s50",
+    autoFinshTime: 1000, // 自动完成改场景
+    enterTrigger: {
+        type: 'last.finish'
+    }
+}, {
+    name: "p4.s60",
+    autoFinshTime: 1000, // 自动完成改场景
+    enterTrigger: {
+        type: 'last.finish'
+    }
+}, {
+    name: "p1",
+    autoFinshTime: 1000, // 自动完成改场景
+    // enterTrigger: {
+    //     type: 'event',
+    //     eventName: 'boy.p2.s0.finish'
+    // }
 }, {
     name: "p3.s3",
     //没有定义 enterTrigger 时 只有在调用 scene.enter(...)进入。不自动处理
