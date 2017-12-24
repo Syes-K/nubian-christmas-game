e5daa3e1d569de264ecabcd016be32f9bf3ec103
ao <dawn@ptmind.com>
2017-12-24 21:13:21 +0800

    hotfix

--- js/boy-page/p11.js
+++ js/boy-page/p11.js
@@ -1,11 +1,6 @@
 function startP11S10(){
     boy.finishGame();
-    // document.querySelector(".p11__btn-1").addEventListener("click", function() {
-    //     scene.next();
-    // })
-}
-function startP11S20(){
-    document.querySelector(".p11__btn-2").addEventListener("click", function() {
+    document.querySelector(".p11__btn-1").addEventListener("click", function() {
         location.href = config.gameGirlUrl;
     })
 }
